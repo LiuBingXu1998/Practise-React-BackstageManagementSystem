977bccbc7af4b4f5e086028534c5e771a5056101
LiuBingXu <lbxemail@icloud.com>
2021-10-06 10:14:33 +0800

    解决自动选中左侧菜单//展开当前列表

--- src/components/leftNav/LeftNav.jsx
+++ src/components/leftNav/LeftNav.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Menu } from 'antd';
 import {
     HomeOutlined,
@@ -21,8 +21,13 @@ const { SubMenu } = Menu;
 /**
  * 左侧导航组件
  */
-export default class LeftNav extends Component {
+class LeftNav extends Component {
     render() {
+        // 获取当前请求路径
+        const path = this.props.location.pathname;
+        // 获取需要展开的列表
+        const openKey = this.getOpenKey(path);
+
         return (
             <div className="left-nav">
                 <Link to="/admin/home" className="header-logo">
@@ -31,40 +36,41 @@ export default class LeftNav extends Component {
                 </Link>
 
                 <Menu
-                    defaultSelectedKeys={['1']}
                     mode="inline"
                     theme="dark"
+                    selectedKeys={[path]}
+                    defaultOpenKeys={[openKey]}
                 >
-                    <Menu.Item key="1" icon={<HomeOutlined />}>
+                    <Menu.Item key="/admin/home" icon={<HomeOutlined />}>
                         <Link to="/admin/home">首页</Link>
                     </Menu.Item>
 
-                    <SubMenu key="sub1" icon={<AppstoreOutlined />} title="商品">
-                        <Menu.Item key="2" icon={<UnorderedListOutlined />}>
+                    <SubMenu key="/products" icon={<AppstoreOutlined />} title="商品">
+                        <Menu.Item key="/admin/category" icon={<UnorderedListOutlined />}>
                             <Link to="/admin/category">品类管理</Link>
                         </Menu.Item>
 
-                        <Menu.Item key="3" icon={<ToolOutlined />}>
+                        <Menu.Item key="/admin/product" icon={<ToolOutlined />}>
                             <Link to="/admin/product">商品管理</Link>
                         </Menu.Item>
                     </SubMenu>
 
-                    <Menu.Item key="4" icon={<UserOutlined />}>
+                    <Menu.Item key="/admin/user" icon={<UserOutlined />}>
                         <Link to="/admin/user">用户管理</Link>
                     </Menu.Item>
 
-                    <Menu.Item key="5" icon={<SafetyCertificateOutlined />}>
+                    <Menu.Item key="/admin/role" icon={<SafetyCertificateOutlined />}>
                         <Link to="/admin/role">角色管理</Link>
                     </Menu.Item>
 
-                    <SubMenu key="sub2" icon={<AreaChartOutlined />} title="图形图表">
-                        <Menu.Item key="6" icon={<BarChartOutlined />}>
+                    <SubMenu key="/charts" icon={<AreaChartOutlined />} title="图形图表">
+                        <Menu.Item key="/admin/charts/bar" icon={<BarChartOutlined />}>
                             <Link to="/admin/charts/bar">柱形图</Link>
                         </Menu.Item>
-                        <Menu.Item key="7" icon={<LineChartOutlined />}>
+                        <Menu.Item key="/admin/charts/line" icon={<LineChartOutlined />}>
                             <Link to="/admin/charts/line">折线图</Link>
                         </Menu.Item>
-                        <Menu.Item key="8" icon={<PieChartOutlined />}>
+                        <Menu.Item key="/admin/charts/pie" icon={<PieChartOutlined />}>
                             <Link to="/admin/charts/pie">饼图</Link>
                         </Menu.Item>
                     </SubMenu>
@@ -72,4 +78,27 @@ export default class LeftNav extends Component {
             </div>
         )
     }
+
+    /**
+     * 获取需要展开的列表对应的key
+     * @param {String} path 当前路径
+     * @returns {String} result 需要展开的key
+     */
+    getOpenKey = (path) => {
+        let result = "";
+
+        const charts = "charts";
+        const productsOne = "category";
+        const peoductsTwo = "product";
+
+        if (path.search(charts) !== -1) {
+            return result = "/charts";
+        }else if (path.search(productsOne) !== -1 || path.search(peoductsTwo) !== -1) {
+            return result = "/products";
+        }else {
+            return result;
+        }
+    }
 }
+
+export default withRouter(LeftNav);
